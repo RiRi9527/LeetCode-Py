34c1e73e484a5d0027a11e96e11f14c9768ffc1f
Hao Zhou <hzhou129527@gmail.com>
2024-08-26 21:32:47 -0400

    0826

--- 03-Longest Substring Without Repeating Characters.py
+++ 03-Longest Substring Without Repeating Characters.py
@@ -17,24 +17,24 @@ s = Solution()
 print(s.lengthOfLongestSubstring("aacdabbbb") )
 
 
-# def length_of_longest_substring(s: str) -> int:
-#     char_index_map = {}
-#     longest = 0
-#     start = 0
+def length_of_longest_substring(s: str) -> int:
+    char_index_map = {}
+    longest = 0
+    start = 0
 
-#     for i, char in enumerate(s):
-#         if char in char_index_map and char_index_map[char] >= start:
-#             start = char_index_map[char] + 1
-#         char_index_map[char] = i
-#         longest = max(longest, i - start + 1)
+    for i, char in enumerate(s):
+        if char in char_index_map and char_index_map[char] >= start:
+            start = char_index_map[char] + 1
+        char_index_map[char] = i
+        longest = max(longest, i - start + 1)
 
-#     return longest
+    return longest
 
-# # Example usage:
-# s1 = "abcabcbb"
-# s2 = "bbbbb"
-# s3 = "pwwkew"
+# Example usage:
+s1 = "abcabcbb"
+s2 = "bbbbb"
+s3 = "pwwkew"
 
-# print(length_of_longest_substring(s1))  # Output: 3
-# print(length_of_longest_substring(s2))  # Output: 1
-# print(length_of_longest_substring(s3))  # Output: 3
+print(length_of_longest_substring(s1))  # Output: 3
+print(length_of_longest_substring(s2))  # Output: 1
+print(length_of_longest_substring(s3))  # Output: 3
